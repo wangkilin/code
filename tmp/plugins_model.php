9e56a5f8afad5c3e0b84ffea15182e94dd469269
niceqi110164 <15210690703@163.com>
2017-12-08 08:18:49 +0800

    lala

--- tmp/plugins_model.php
+++ tmp/plugins_model.php
@@ -1,1 +1,1 @@
-a:2:{s:18:"aws_external_class";s:69:"/Users/zhoumingxia/icodebang.cn/plugins/aws_external/aws_external.php";s:26:"aws_offical_external_class";s:85:"/Users/zhoumingxia/icodebang.cn/plugins/aws_offical_external/aws_offical_external.php";}
+a:2:{s:18:"aws_external_class";s:46:"D:\icode/plugins/aws_external/aws_external.php";s:26:"aws_offical_external_class";s:62:"D:\icode/plugins/aws_offical_external/aws_offical_external.php";}
