cfe2ab1d8aa2006e559a0a99a47bea1bb706052e
niceqi110164 <15210690703@163.com>
2017-12-10 21:10:25 +0800

    html根标签加font-size和js

--- views/default/m/english/header_meta.php
+++ views/default/m/english/header_meta.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html style="font-size:100px;">
 <head>
 	<meta charset="utf-8" />
 	<meta name="robots" content="noarchive" />
@@ -44,6 +44,22 @@
 		var G_QUICK_PUBLISH_HUMAN_VALID = true;
 		<?php } ?>
 	</script>
+
+     <script type="text/javascript">
+             !function(n){
+                 var e=n.document,
+                         t=e.documentElement,
+                         i=720,
+                         d=i/100,
+                         o="orientationchange"in n?"orientationchange":"resize",
+                         a=function(){
+                             var n=t.clientWidth||320;n>720&&(n=720),
+                                     t.style.fontSize=n/d+"px"
+                         };
+                 e.addEventListener&&(n.addEventListener(o,a,!1),
+                         e.addEventListener("DOMContentLoaded",a,!1))
+             }(window);
+         </script>
 
 	<?php if (is_array($this->_import_js_files)) { ?>
 	<?php foreach ($this->_import_js_files AS $import_js) { ?>
