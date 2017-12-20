127bf59e21e409cdfd393116c2eb7cfb43947cf3
niceqi110164 <15210690703@163.com>
2017-12-20 20:51:25 +0800

    my

--- views/default/m/english/home.php
+++ views/default/m/english/home.php
@@ -1,18 +1,27 @@
 <?php View::output('m/english/header.php'); ?>
 	<div class="container">
-      <div>
-        <div>
-          <p><span>累计学习纪录获得xxx积分</span></p>
-        </div>
-        <div>
-          <p><span>连续学习纪录获得xxx积分          </span></p>
-        </div>
-      </div>
-      <div id="u12" class="ax_shape">
-        <div id="u13" class="text" style="top: 18px; transform-origin: 138px 7px 0px;">
-          <p><span>积分商城？？</span></p>
-        </div>
-      </div>
+       <div class="my-points">
+          <div class="m-left-points">
+             <div class="l-points-text">
+                <p>CREDITS</p>
+                <p>累计学习记录获得积分</p>
+                <p>积分 <span>193</span></p>
+             </div>
+          </div>
+
+          <div class="m-right-points">
+             <div class="r-points-text">
+                <p>CREDITS</p>
+                <p>连续学习记录获得积分</p>
+                <p>积分 <span>193</span></p>
+             </div>
+          </div>
+       </div>
+       <div id="u12" class="ax_shape">
+          <div id="u13" class="text" style="top: 18px; transform-origin: 138px 7px 0px;">
+             <p><span>积分商城？？</span></p>
+          </div>
+       </div>
 
       <div>
           <a href="">已购课程</a>
