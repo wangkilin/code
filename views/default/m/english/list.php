6a7b840ea58c075ac7b1ec10b0d221ed8c56749f
niceqi110164 <15210690703@163.com>
2017-12-20 20:18:59 +0800

    list

--- views/default/m/english/list.php
+++ views/default/m/english/list.php
@@ -6,10 +6,10 @@
 	    <div class="search_wrap">
 	      <form class="navbar-search" action="m/english/list/category-<?php echo $_GET['category'];?>" id="course_search_form" method="post">
               <input class="form-control search-query" type="text" placeholder="<?php _e('搜索问题、话题或人'); ?>" autocomplete="off" name="q" id="icb-search-query" data-dropdown-type="tip"/>
-              <span title="<?php _e('搜索'); ?>" id="global_search_btns" onClick="$('#course_search_form').submit();"><i class="icon icon-search"></i></span>
+              <span class="search-icon" title="<?php _e('搜索'); ?>" id="global_search_btns" onClick="$('#course_search_form').submit();"><i class="icon icon-search"></i></span>
           </form>
 	    </div>
-		<ul>
+		<ul class="list-content">
 		  <?php foreach ($this->list as $val) {?>
 			<?php if ($val['title']) { ?>
 			<li class="col-sm-12">
@@ -17,12 +17,21 @@
 				  <span><?php echo $val['title']; ?></span>
 				  <img src="<?php echo getMudulePicUrlBySize('category', 'max', $val['pic']);?>"/>
 				</a>
-				<span>
-				  <i class="teacher_icon">头像</i>
-				  <u class="teacher_brief">Maggie：美国本土小学教师</u>
-				  <u class="course_views"><?php echo _t('收听人数'), ':', $val['views'];?></u>
-				  <u class="course_time"><?php echo date('Y/m/d', $val['add_time']);?></u>
-				</span>
+				<div class="list-info">
+                    <div class="list-info-left">
+                        <i class="teacher_icon"></i>
+                    </div>
+                    <div class="list-info-right">
+                        <p class="teacher_brief">Maggie：美国本土小学教师</p>
+                        <p>
+                            <span class="course_views">
+                                <?php echo _t('收听人数'), ':', $val['views'];?>
+                            </span>
+                            <span class="course_time"><?php echo date('Y/m/d', $val['add_time']);?></span>
+                        </p>
+
+                    </div>
+				</div>
 			</li>
 			<?php } ?>
 		  <?php } ?>
