2fb4175580029bfed2c56e00df22328ddfe9f0e1
niceqi110164 <15210690703@163.com>
2017-12-20 21:53:29 +0800

    my页面

--- views/default/m/english/home.php
+++ views/default/m/english/home.php
@@ -2,7 +2,7 @@
 	<div class="container">
        <div class="my-points">
           <div class="m-left-points">
-             <div class="l-points-text">
+             <div class="points-text">
                 <p>CREDITS</p>
                 <p>累计学习记录获得积分</p>
                 <p>积分 <span>193</span></p>
@@ -10,24 +10,46 @@
           </div>
 
           <div class="m-right-points">
-             <div class="r-points-text">
+             <div class="points-text">
                 <p>CREDITS</p>
                 <p>连续学习记录获得积分</p>
                 <p>积分 <span>193</span></p>
              </div>
           </div>
        </div>
-       <div id="u12" class="ax_shape">
-          <div id="u13" class="text" style="top: 18px; transform-origin: 138px 7px 0px;">
-             <p><span>积分商城？？</span></p>
-          </div>
-       </div>
+        <div id="u12" class="ax_shape">
+           <div id="u13" class="text" style="top: 18px; transform-origin: 138px 7px 0px;">
+              <p><span>积分商城？？</span></p>
+           </div>
+        </div>
 
-      <div>
-          <a href="">已购课程</a>
-          <a href="">收听历史</a>
-          <a href="">修改密码</a>
-          <a href="">修改昵称</a>
-      </div>
+        <!-- 选项部分-->
+        <div class="my-option">
+            <ul class="option-box">
+                <li>
+                    <div class="option-item pay-course">
+                        <a href="">已购课程</a>
+                    </div>
+                </li>
+                <li>
+                    <div class="option-item listen-history">
+                        <a href="">收听历史</a>
+                    </div>
+                </li>
+                <li>
+                    <div class="option-item change-pwd">
+                        <a href="">修改密码</a>
+                    </div>
+                </li>
+                <li>
+                    <div class="option-item change-nickname">
+                        <a href="">修改密码</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+
+
+
     </div>
 <?php View::output('m/english/footer.php'); ?>
