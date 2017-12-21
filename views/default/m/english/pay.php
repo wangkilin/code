ccd65e96e7ddb2fc5370e8f46b8049f799fc67ea
niceqi110164 <15210690703@163.com>
2017-12-21 15:02:14 +0800

    pay页面

--- views/default/m/english/pay.php
+++ views/default/m/english/pay.php
@@ -1,8 +1,33 @@
 <?php View::output('m/english/header.php'); ?>
-	<div class="container member-intro-img">
-	</div>
-	<div class="container member-intro-text">
-		<span>会员介绍图片文字信息, 会员介绍图片文字信息, 会员介绍图片文字信息
-		<br/>会员介绍图片文字信息</span>
-	</div>
+    <!--内容-->
+    <div class="container">
+        <div class="member-intro-img">
+            <div class="member-text">
+                <p class="top">HELLO</p>
+                <p class="bom">MEMBER</p>
+            </div>
+	    </div>
+	    <div class="member-intro-text">
+		    <h2>
+		        <i class="icon icon-ul"></i>
+                付款信息
+                <span><b>I</b>NFORMATION</span>
+		    </h2>
+		    <ul class="member-info">
+                <li>
+                    <div class="member">
+                        <i class="icon icon-cogs"></i>
+                        会员信息
+                    </div>
+                </li>
+                <li>
+                    <div class="price">价格</div>
+                </li>
+                <li>
+                    <div class="summarize">课程概括</div>
+                </li>
+            </ul>
+	    </div>
+    </div>
+
 <?php View::output('m/english/foot_pay.php'); ?>
