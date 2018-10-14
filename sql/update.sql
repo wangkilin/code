05197cefdf3e5ca33795bd160036a1f10e71de35
icodebang <icodebang@126.com>
2018-10-14 22:52:04 +0800

    [code] 内容模块设置

---
+++ sql/update.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS `icb_post_module`;
+CREATE TABLE `icb_post_module` (
+  `id` int(11) NOT NULL COMMENT 'id',
+  `title` varchar(20) NOT NULL COMMENT '可发布内容的模块名称',
+  `url_token` varchar(20) NOT NULL COMMENT '模块别名',
+  `notes` text NOT NULL COMMENT '备注信息'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='支持发布内容的模块列表';
+
+ALTER TABLE `icb_post_module`
+  ADD PRIMARY KEY (`id`);
+ALTER TABLE `icb_post_module`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id';
--- views/default/admin/category/module.php
+++ views/default/admin/category/module.php
@@ -13,13 +13,58 @@
             <div class="alert alert-success collapse error_message"></div>
 
             <div class="table-responsive">
-            待开发： 分类属于模块； 模块绑定到控制器名称； 在控制器中调用分类时， 自动加载本模块分类；
-            <br/>
-            <br/>
-            另外， 模板页面是否需要引入layout概念？ 当前是每个页面都将头尾信息引入。
-            <br/>
-            引入layout概念后，每个页面只关注本页面自己的内容。 公共内容在layout中定义。
-
+                <table class="table table-striped">
+                    <thead>
+                    <tr>
+                        <th><?php _e('模块标题'); ?></th>
+                        <th><?php _e('排序'); ?></th>
+                        <th><?php _e('操作'); ?></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <form id="category_form" action="admin/ajax/save_category_sort/" method="post" onsubmit="return false">
+                    <?php foreach ($this->list AS $key => $val) { ?>
+                    <tr>
+                        <td>
+                            <a href="index/category-<?php echo ($val['url_token']) ? $val['url_token'] : $val['id']; ?>"><?php echo $val['title']; ?></a>
+                        </td>
+                        <td>
+                            <div class="col-sm-6 clo-xs-12 col-lg-offset-3">
+                                <input type="text" class="form-control sort-action" name="category[<?php echo $val['id']; ?>][sort]" value="<?php echo $val['sort']; ?>" />
+                            </div>
+                        </td>
+                        <td >
+                            <a href="admin/category/edit/category_id-<?php echo $val['id']; ?>" data-toggle="tooltip" class="icon icon-edit md-tip" title="<?php _e('编辑'); ?>"></a>
+                            <a onclick="AWS.ajax_request(G_BASE_URL + '/admin/ajax/remove_category/', 'category_id=<?php echo $val['id'];?>');" data-toggle="tooltip" class="icon icon-trash md-tip" title="<?php _e('删除'); ?>"></a>
+                            <a data-id="<?php echo $val['id']; ?>" data-name="<?php echo $val['title']?>" data-url="admin/category/move_contents/category_id-" data-toggle="tooltip" class="icon icon-transfer md-tip move" title="<?php _e('批量移动'); ?>"></a>
+                        </td>
+                    </tr>
+                    <?php } ?>
+                    </form>
+                    </tbody>
+                    <tfoot class="mod-foot-center">
+                    <tr>
+                        <td colspan="3">
+                        <form id="add_module_form" action="admin/ajax/save_module/" method="post" onsubmit="return false">
+                            <div class="form-group col-sm-5">
+                                <span  class="col-sm-3 col-xs-12 mod-category-foot"><?php _e('模块标题'); ?></span>
+                                <div class="col-sm-6 col-xs-12">
+                                    <input type="text" class="form-control" name="title" />
+                                </div>
+                            </div><div class="form-group col-sm-5">
+                                <span  class="col-sm-3 col-xs-12 mod-category-foot"><?php _e('别名'); ?></span>
+                                <div class="col-sm-6 col-xs-12">
+                                    <input type="text" class="form-control" name="url_token" />
+                                </div>
+                            </div>
+                            <div class="col-sm-2 col-xs-12">
+                             <a onclick="ICB.ajax.postForm($('#add_module_form'));" class="btn-primary btn"><?php _e('添加模块'); ?></a>
+                            </div>
+                        </form>
+                        </td>
+                    </tr>
+                    </tfoot>
+                </table>
             </div>
         </div>
     </div>
