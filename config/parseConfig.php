4e24d7bebe997ec723329dedd67cb8d9fccde83c
icodebang <icodebang@126.com>
2020-04-05 21:39:33 +0800

    metal parser config

--- config/parseConfig.php
+++ config/parseConfig.php
@@ -5,4 +5,10 @@ $config['metal'] = array(
     'url_prefix'    => 'https://hq.smm.cn',
 );
 
+
+defined('ROOT_PATH') OR define('ROOT_PATH', realpath(dirname(__DIR__)) . DIRECTORY_SEPARATOR);
+
+// 配置文件路径
+define('CONF_PATH', ROOT_PATH . 'config' . DIRECTORY_SEPARATOR);
+
 return $config;
