1bea9ab68126d3395b6db0a30b2f4c397607d932
icodebang <icodebang@126.com>
2021-02-08 10:45:29 +0800

    [code] icodebang.cn 加入百度统计

--- views/default/global/footer.php
+++ views/default/global/footer.php
@@ -79,28 +79,25 @@ $('div.code>div,div.content div.sample-code-container').each(function(i, block) 
     hljs.configure({useBR: $(this).find('br').length>0});
     hljs.highlightBlock(block);
 });
-<?php if (strpos($_SERVER['HTTP_HOST'], 'icodebang.com')) { ?>
-// icodebang.com 百度统计
+<?php if (strpos($_SERVER['HTTP_HOST'], 'icodebang.cn')) {
+    $baiduStatCode = "be8b971fb0f380a3005b896a533a9cb2";
+} else if (strpos($_SERVER['HTTP_HOST'], 'icodebang.com')) {
+    $baiduStatCode = "be8b971fb0f380a3005b896a533a9cb2";
+} else if (strpos($_SERVER['HTTP_HOST'], 'devboy.cn')) {
+    $baiduStatCode = "bb69e584fbcae17dd29f6c1fdaca4fc1";
+}
+
+if ($baiduStatCode) {
+?>
+// 百度统计
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?681fc9b1c75c25b5868d6bfdea94f7df";
+  hm.src = "https://hm.baidu.com/hm.js?<?php echo $baiduStatCode;?>";
   var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(hm, s);
 })();
-<?php }
-if (strpos($_SERVER['HTTP_HOST'], 'devboy.cn')) {
-?>
-// devboy.cn 百度统计
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?bb69e584fbcae17dd29f6c1fdaca4fc1";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s);
-})();
-<?php }
-?>
+<?php }?>
 // 百度自动推送
 (function(){
     var bp = document.createElement('script');
