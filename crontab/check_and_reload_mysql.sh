3e47dc0c09d500d42464ca21eb4ec4be608bac78
icodebang <icodebang@126.com>
2021-06-07 17:29:14 +0800

    [code] 添加检查mysql表的命令参数

--- crontab/check_and_reload_mysql.sh
+++ crontab/check_and_reload_mysql.sh
@@ -2,7 +2,7 @@
 
 IS_MYSQL_RUNNING=`/bin/systemctl status  mysql.service | grep active | grep running | wc -l`
 if [[ $IS_MYSQL_RUNNING = 0 ]]; then
-    myisamchk -f /var/lib/mysql/WeCenter/icb_article.MYI
+    myisamchk -o -f /var/lib/mysql/WeCenter/icb_article.MYI
     /bin/systemctl start  mysql.service
 
 fi
