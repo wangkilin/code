50de6bc4ebd226e1532c942c571b097de86af648
icodebang <icodebang@126.com>
2021-06-07 17:21:11 +0800

    [code] mysql check service

--- crontab/check_and_reload_mysql.sh
+++ crontab/check_and_reload_mysql.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
 IS_MYSQL_RUNNING=`/bin/systemctl status  mysql.service | grep active | grep running | wc -l`
-if [[ $IS_MYSQL_RUNNING -ne 1 ]]; then
+if [[ $IS_MYSQL_RUNNING = 0 ]]; then
     myisamchk -f /var/lib/mysql/WeCenter/icb_article.MYI
     /bin/systemctl start  mysql.service
 
