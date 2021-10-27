a72c6adb64784e57fcbd454f339b0a989088f2d9
icodebang <icodebang@126.com>
2021-10-27 09:07:01 +0800

    [optim] 增加代码注释

--- crontab/export_article_data.sh
+++ crontab/export_article_data.sh
@@ -6,18 +6,18 @@ DIR=${BASH_SOURCE%/*} # /dir1/dir2/dir3
 
 . $DIR/global.config.sh  # 引入配置
 
-# 获取服务器端
+# 获取服务器端的文章最大的id值。 根据这个id值， 获取到最新的数据。
 scp -P $SSH_PORT $SSH_USERNAME@$SSH_HOST:$SSH_STORE_DIR/MAX_ARTICLE_ID.txt $LOCAL_STORE_DIR/ > /dev/null 2>&1;
 
 scp -P $SSH_PORT $SSH_USERNAME@$SSH_HOST:$SSH_STORE_DIR/MAX_ARTICLE_POST_ID.txt $LOCAL_STORE_DIR/ > /dev/null 2>&1;
-
+# 如果没有从服务器端获取到文章id数据，直接退出
 if [ ! -e $LOCAL_STORE_DIR/MAX_ARTICLE_ID.txt ]; then
     exit 0
 fi
 if [ ! -e $LOCAL_STORE_DIR/MAX_ARTICLE_POST_ID.txt ]; then
     exit 0
 fi
-
+# 从文件中获取到id值
 ARTICLE_ID=`tail -1 $LOCAL_STORE_DIR/MAX_ARTICLE_ID.txt`
 ARTICLE_POST_ID=`tail -1 $LOCAL_STORE_DIR/MAX_ARTICLE_POST_ID.txt`
 
@@ -28,12 +28,12 @@ fi
 if [[ $ARTICLE_POST_ID -le 100000 ]]; then
     exit 0
 fi
-
+# id值已经获取到， 删除对饮的文件
 rm -f $LOCAL_STORE_DIR/MAX_ARTICLE_ID.txt $LOCAL_STORE_DIR/MAX_ARTICLE_POST_ID.txt
 
-
+# 根据是否设置了数据库密码，使用不用的命令
 if [[ "$DB_PASSWORD" = "" ]]; then
-    # 1. 数据库备份
+    # 1. 数据库备份, 获取到 > id 的数据
     /usr/local/bin/mysqldump -h $DB_HOST -u $DB_USERNAME $DB_DATABASE icb_posts_index --column-statistics=0 --no-create-info  -w "post_id>${ARTICLE_POST_ID}"  > $LOCAL_STORE_DIR/article_post_great_than_${ARTICLE_POST_ID}.sql
     /usr/local/bin/mysqldump -h $DB_HOST -u $DB_USERNAME $DB_DATABASE icb_article --column-statistics=0 --no-create-info  -w "id>${ARTICLE_ID}"  > $LOCAL_STORE_DIR/article_great_than_${ARTICLE_ID}.sql
 else
@@ -46,11 +46,11 @@ cd $LOCAL_STORE_DIR
 tar zcf $LOCAL_STORE_DIR/article_great_than_${ARTICLE_ID}.tgz  article_great_than_${ARTICLE_ID}.sql article_post_great_than_${ARTICLE_POST_ID}.sql
 # 1.2 打包后，将刚生成的sql文件删除，只保留打包文件，释放空间。
 rm -f $LOCAL_STORE_DIR/article_great_than_${ARTICLE_ID}.sql $LOCAL_STORE_DIR/article_post_great_than_${ARTICLE_POST_ID}.sql
-
+# 将打包的数据文件包远程复制到服务器
 scp -P $SSH_PORT $LOCAL_STORE_DIR/article_great_than_${ARTICLE_ID}.tgz $SSH_USERNAME@$SSH_HOST:$SSH_STORE_DIR/
 
 
-# 2. 删除7点前的打包文件， 释放空间
+# 2. 删除打包文件， 释放空间
 if [ -e $LOCAL_STORE_DIR/article_great_than_${ARTICLE_ID}.tgz ]; then
     rm -f $LOCAL_STORE_DIR/article_great_than_${ARTICLE_ID}.tgz
 fi
--- crontab/import_article_data.sh
+++ crontab/import_article_data.sh
@@ -5,9 +5,9 @@
 DIR=${BASH_SOURCE%/*} # /dir1/dir2/dir3
 
 . $DIR/global.config.sh  # 引入配置
-
+# 生成存放文章最大id的文件
 source $DIR/export_icodebang_article_max_id.sh
-
+# 如果没有生成对应的存放id文件， 直接退出
 if [ ! -e $SSH_STORE_DIR/MAX_ARTICLE_ID.txt ]; then
     echo "No article id found"
     exit 0
@@ -16,7 +16,7 @@ if [ ! -e $SSH_STORE_DIR/MAX_ARTICLE_POST_ID.txt ]; then
     echo "No article post id found"
     exit 0
 fi
-
+# 获取到文章id
 ARTICLE_ID=`tail -1 $SSH_STORE_DIR/MAX_ARTICLE_ID.txt`
 ARTICLE_POST_ID=`tail -1 $SSH_STORE_DIR/MAX_ARTICLE_POST_ID.txt`
 
@@ -28,17 +28,17 @@ if [[ $ARTICLE_POST_ID -le 100000 ]]; then
     exit 0
 fi
 
-
+# 对应的数据包不存在， 直接退出
 if [ ! -e $SSH_STORE_DIR/article_great_than_${ARTICLE_ID}.tgz ]; then
     echo "No article data tgz found"
     exit 0
 fi
-
+# 切换到文件存放目录， 方便下一步解压文件
 cd $SSH_STORE_DIR
-
+# 加压文件， 删除包
 tar zxf $SSH_STORE_DIR/article_great_than_${ARTICLE_ID}.tgz
 rm -f $SSH_STORE_DIR/article_great_than_${ARTICLE_ID}.tgz
-
+# 判断是否解压出了对应的数据文件， 如果不存在数据文件，直接退出
 if [ ! -e $SSH_STORE_DIR/article_great_than_${ARTICLE_ID}.sql ]; then
     echo "No article sql found"
     exit 0
@@ -47,7 +47,7 @@ if [ ! -e $SSH_STORE_DIR/article_post_great_than_${ARTICLE_POST_ID}.sql ]; then
     echo "No article post sql found"
     exit 0
 fi
-
+# 根据数据库是否设置了密码， 执行不同的命令, 导入数据
 if [[ "$DB_PASSWORD" = "" ]]; then
     # 1. 数据库备份
 
@@ -62,11 +62,11 @@ else
     source $SSH_STORE_DIR/article_great_than_${ARTICLE_POST_ID}.sql;
     source $SSH_STORE_DIR/article_post_great_than_${ARTICLE_ID}.sql;
 EOF
-
+# 删除sql文件
 rm -f $SSH_STORE_DIR/article_great_than_${ARTICLE_ID}.sql $SSH_STORE_DIR/article_post_great_than_${ARTICLE_POST_ID}.sql
 
 fi
-
+# 导入数据后， 重新获取并生成文章最大id
 . $DIR/export_icodebang_article_max_id.sh
 
 # 退出
