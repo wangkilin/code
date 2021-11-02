8f681e30309640e1d2e1072c0a978ac413a54965
icodebang <icodebang@126.com>
2021-11-02 09:07:33 +0800

    [optim] 删除多余的crontab目录

--- crontab/check_and_reload_mysql.sh
+++
@@ -1,8 +0,0 @@
-#/bin/sh
-
-IS_MYSQL_RUNNING=`/bin/systemctl status  mysql.service | grep active | grep running | wc -l`
-if [[ $IS_MYSQL_RUNNING = 0 ]]; then
-    myisamchk -o -f /var/lib/mysql/WeCenter/icb_article.MYI
-    /bin/systemctl start  mysql.service
-
-fi
