5f00364c511eb42fe3bc98e7eb7c3ba18c05d7a2
icodebang <icodebang@126.com>
2022-03-01 14:38:03 +0800

    [code] 百度统计，增加域名

--- views/default/global/baidu_stat.php
+++ views/default/global/baidu_stat.php
@@ -5,6 +5,18 @@
     $baiduStatCode = "681fc9b1c75c25b5868d6bfdea94f7df";
 } else if (strpos($_SERVER['HTTP_HOST'], 'devboy.cn')) {
     $baiduStatCode = "bb69e584fbcae17dd29f6c1fdaca4fc1";
+} else if (strpos($_SERVER['HTTP_HOST'], 'kinful.com')) {
+    $baiduStatCode = "2f76f17fb0ba37097de30a8552b76a11";
+} else if (strpos($_SERVER['HTTP_HOST'], 'teacup.com.cn')) {
+    $baiduStatCode = "eecfca100aa19c14ceed1e271e00d47a";
+} else if (strpos($_SERVER['HTTP_HOST'], 'scanonly.com')) {
+    $baiduStatCode = "57b91a3d0d94a5ad2bf47748512edf40";
+} else if (strpos($_SERVER['HTTP_HOST'], '3dwindy.com')) {
+    $baiduStatCode = "f8ff896c8113089b4866edbf81ae0ce1";
+} else if (strpos($_SERVER['HTTP_HOST'], 'ekotlin.com')) {
+    $baiduStatCode = "e1e8b8e705799181727e5d81aecf2fd2";
+} else if (strpos($_SERVER['HTTP_HOST'], 'ukotlin.com')) {
+    $baiduStatCode = "d6d835d2bfa79eaf1afa7c1fe5bbe293";
 }
 if ($baiduStatCode) {
 ?>
