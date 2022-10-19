8660f8c59a1248790665073619dce1f3a45316d5
icodebang <icodebang@126.com>
2022-10-19 09:26:24 +0800

    [optim]

--- views/default/article/index.php
+++ views/default/article/index.php
@@ -77,7 +77,7 @@
 
                             <div class="meta clearfix">
                                 <?php if ($this->article_info['author']!=='') echo '<span>作者:', $this->article_info['author'],'</span>';?>
-                                <?php if ($this->article_info['source_url']!=='') echo '<span>原文地址:', $this->article_info['source_url'],'</span>';?>
+                                <?php if ($this->article_info['source_url']!=='' && !stripos($this->article_info['source_url'], '//ju') && !($this->user_info['permission']['edit_article'] OR $this->user_info['permission']['is_administortar'] OR $this->user_info['permission']['is_moderator'])) echo '<span>原文地址:', $this->article_info['source_url'],'</span>';?>
                                 <?php if ($this->article_info['copy_from']!=='' && ($this->user_info['permission']['edit_article'] OR $this->user_info['permission']['is_administortar'] OR $this->user_info['permission']['is_moderator']) ) echo '<br/><span>来源:<a href="'.$this->article_info['copy_from'].'" target="blank">', $this->article_info['copy_from'],'</a></span>';?>
                             </div>
                             <!-- added internal link, to improve search engine Index data -->
