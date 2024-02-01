380d3c624085687611e4b9f6cdf6046e7893c8e7
icodebang <icodebang@126.com>
2024-02-01 13:57:09 +0800

    [code] 新禾： 工作量提交页面样式调整

--- views/isinho.com/default/admin/workload/fill.php
+++ views/isinho.com/default/admin/workload/fill.php
@@ -254,7 +254,8 @@
                                 <div class="col-sm-11 icb-item-title">
                                     <input type="text" name="remarks" value="<?php echo $this->itemInfo['remarks']; ?>" class="form-control md-tip" data-default="<?php echo $this->bookInfo['remarks']; ?>" title="" data-toggle="tooltip" />
                                 </div>
-                                <div class="col-sm-11 col-sm-offset-1 icb-item-title text-left">
+                                <div class="col-sm-1"></div>
+                                <div class="col-sm-11 icb-item-title text-left">
                                     <?php echo $this->bookInfo['remarks']; ?>
                                 </div>
 
