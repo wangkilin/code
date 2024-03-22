eead028a1c9f1988630a78d3d12e30c56a563272
icodebang <icodebang@126.com>
2024-03-22 11:44:22 +0800

    [optim] 优化异常访问提示内容

--- views/default/global/click_and_reload.php
+++ views/default/global/click_and_reload.php
@@ -1,17 +1,9 @@
 <!doctype html><html><head><meta charset="utf-8" /><title><?php _e('异常访问'); ?></title>
 <script src="<?php echo G_STATIC_URL; ?>/js/jquery.2.js" type="text/javascript"></script>
-<!--[if lte IE 8]>
-	<script type="text/javascript" src="<?php echo G_STATIC_URL; ?>/js/respond.js"></script>
-	<![endif]-->
 </head>
 <body class="icb-404">
-	<div class="icb-404-wrap container">
-		<div class="row">
-			<p><?php _e('异常访问'); ?>，<?php _e('请检查是否存在恶意访问行为！'); ?></p>
-            <br/>
-            <p id="reload_link_pool"><a><?php _e('继续访问'); ?></a><a><?php _e('继续访问'); ?></a><a><?php _e('继续访问'); ?></a></p>
-		</div>
-	</div>
+<p><?php _e('监测到异常行为！'); ?></p><br/>
+<p id="reload_link_pool" style="text-align:center;"><a><?php _e('继续访问'); ?></a><a><?php _e('继续访问'); ?></a><a><?php _e('继续访问'); ?></a></p>
 <script type="text/javascript">
 function clickReload(){window.location.reload();}
 $(function(){
@@ -23,7 +15,4 @@ $(function () {/* 防止手机浏览器在页面后面加入垃圾广告 */
 $(document).scroll(function () {$("#last-one-flag").nextAll(':not(#scrollUp,.datetimepicker)').remove();});
 });
 </script>
-<!-- 防止手机浏览器在页面后面加入垃圾广告, 放置一个标志性的元素。 所有必须的元素， 都要放到这个标识元素前面  -->
-<div id="last-one-flag"></div>
-</body>
-</html>
+<div id="last-one-flag"></div></body></html>
