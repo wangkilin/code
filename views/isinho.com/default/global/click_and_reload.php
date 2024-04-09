f25970e545b674469671bd2dcd2f5404e5eb4498
icodebang <icodebang@126.com>
2024-04-09 13:44:22 +0800

    [bug] 新禾：访问新禾页面，mac跳转没有对应的模板页

---
+++ views/isinho.com/default/global/click_and_reload.php
@@ -0,0 +1,5 @@
+<!doctype html><html><head><meta charset="utf-8" /><title><?php _e('加载中'); ?></title>
+</head>
+<body><script type="text/javascript">
+(function(){setTimeout(() => {window.location.reload();}, 100);})();
+</script></body></html>
